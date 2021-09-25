7de1fb2091d8ac3de4b298ff4ca3d3b1faa26b18
Alex Rosa <alexsros@gmail.com>
2021-09-25 12:39:28 +0100

    Init project app-sa-service

---
+++ src/main/kotlin/pt/app/sa/service/AppSaServiceApplication.kt
@@ -0,0 +1,11 @@
+package pt.app.sa.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppSaServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AppSaServiceApplication>(*args)
+}
---
+++ src/test/kotlin/pt/app/sa/service/AppSaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.app.sa.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppSaServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
